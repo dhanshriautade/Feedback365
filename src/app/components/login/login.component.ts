7496f7985f359ab90014d41768248394259591ae
dhanshriautade <dhanshri.autade3@gmail.com>
2019-12-14 12:46:25 +0530

    changes made rani

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/components/services/auth.service';
 import { FormControl, FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
-import { CompileShallowModuleMetadata } from '@angular/compiler';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,44 +9,56 @@ import { CompileShallowModuleMetadata } from '@angular/compiler';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+
+  })
+  submitted = false;
   activeAllUsers = false;
-  loginForm: FormGroup;
+
   role;
   data;
-  constructor(private formBuilder: FormBuilder, public AuthService: AuthService) { }
+  constructor(private formBuilder: FormBuilder, public AuthService: AuthService, private router: Router) { }
 
 
   myTeamView(flag) {
     // this.activeAllUsers = false;
     if (flag === 1) {
-        this.activeAllUsers = true;
-        this.role= 'employee';
+      this.activeAllUsers = true;
+      this.role = 'employee';
     }
-    else{
+    else {
       this.activeAllUsers = false;
-      this.role= 'admin';
+      this.role = 'admin';
     }
-    
+
   }
-  Login(){
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required,
+      Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]],
+    });
+  }
+  // convenience getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
+  Login() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
 
     this.data = {
       "email": this.loginForm.value.email,
       "password": this.loginForm.value.password,
-      "role" : this.role
+      "role": this.role
     }
     this.AuthService.LogIn(this.data).subscribe(res => {
       console.log(res);
 
-  })
-     console.log(this.data);
+    })
   }
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      email: [''],
-      password: [''],
-      
-    });
-  }
-
 }
