d4b724ca7429a4ce70564a9e7bd447c755579adc
dhanshriautade <dhanshri.autade2#gmail.com>
2019-12-18 10:30:24 +0530

    changes

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from 'src/app/components/services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
@@ -7,7 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
   display: boolean = false;
-  constructor() { }
+  name;
+  designation;
+  empid;
+  totalvalue;
+  constructor(public AuthService: AuthService,private router: Router) { 
+    this.name = localStorage.getItem('name');
+    this.designation= localStorage.getItem('Designation');
+    this.empid = localStorage.getItem('id');
+    this.AuthService.GetTotalEvent().subscribe(res => {
+      this.totalvalue = res;
+      // console.log('lsist', this.totalvalue);
+    })
+  }
+
+  showfeedback(){
+    this.router.navigateByUrl('/main/feedback');
+  }
 
   ngOnInit() {
   }
